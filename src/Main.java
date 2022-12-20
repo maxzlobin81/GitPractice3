772bd2441f8657ee821556b97e7b090acea0b78f
maxzlobin81 <maxzlobin@gmail.com>
2022-12-20 07:55:39 -0600

    another change in Max's branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Max now really working on git practice 3");
+        System.out.println("Another change made");
     }
 }
