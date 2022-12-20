73be64eeebd437028ab9df8758490c55a22f21c3
maxzlobin81 <maxzlobin@gmail.com>
2022-12-20 08:07:11 -0600

    added another feature on Max2 branch

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Max now really working on git practice 3");
         System.out.println("Another change made");
         System.out.println("Implementing new feature on Max2 branch");
+        System.out.println("Another feature added on Max2");
     }
 }
